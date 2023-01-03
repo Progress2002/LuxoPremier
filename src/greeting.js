86bbeca5e139b1ab96227297c2385bac40b9f99d
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-01-03 13:54:08 +0100

    Create header section

--- src/greeting.js
+++
@@ -1,3 +0,0 @@
-const greeting = (name) => `Hello ${name}!`;
-
-export default greeting;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
 import './style.css';
-import greeting from './greeting.js';
-
-const h1 = document.querySelector('h1');
-h1.innerHTML = greeting('John Doe');
