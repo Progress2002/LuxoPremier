5da8e21f512b74751f4f69dcafa6cbe4eb1761e3
kennedyowusu <kennediowusu@gmail.com>
2023-01-03 09:37:02 +0000

    fix eslint error

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 import _ from 'lodash';
 import './style.css';
 
@@ -5,6 +6,7 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
+  // eslint-disable-next-line no-undef
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
